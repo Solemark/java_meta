f1d6a36a0fb33253ace02cbdf2eee11881c4ea4e
Solemark <larco9mason@gmail.com>
2023-05-24 17:55:15 +1000

    fixed the cringe from first ever code assignment

--- first_java_application/Week2.java
+++ first_java_application/Week2.java
@@ -1,32 +1,19 @@
-/*
-Programmer: Mason Larcombe s0257989
-File: Week2
-Date: 13/03/2017
-Purpose: COIT11222 assignment one question two for Term 1 2017
-using basic input/output create a program to calculate a students' percentage mark.
-*/
-import java.util.Scanner;														//Imported object/utility scanner for keyboard prompt
-public class Week2
-{
-    public static void main(String[] args)
-    {
-        final int max_marks = 65;												//Maximium mark variable
-        String name;															//Student name variable
-        int mark,p;																//Student mark and percentage mark variables
+import java.util.Scanner;
 
-        Scanner kb=new Scanner(System.in);										//Activates keyboard prompt (kb)
+public class Week2 {
+    public static void main(String[] args) {
+        final int max_marks = 65;
+        String name;
+        int mark, total;
 
-        System.out.print("Enter the name of the student: ");					//Asks for student name
-        name=kb.nextLine();														//Retrieves student name as string
-		System.out.println("");													//Line break
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter the name of the student: ");
+            name = input.next();
 
-        System.out.print("Enter of mark of "+name+" out of "+max_marks+": ");	//Asks for the students mark out of integer "max_marks"
-        mark=kb.nextInt();														//Retrieves student mark as integer
-		System.out.println("");													//Line break
-
-        p=(mark*100)/max_marks;													//Calculates students percentage
-
-        System.out.println(name+" recieved "+p+"% of total marks.");			//Outputs student name and percentage marks
-        System.out.println("");													//Line break
+            System.out.print("Enter of mark of " + name + " out of " + max_marks + ": ");
+            mark = input.nextInt();
+        }
+        total = (mark * 100) / max_marks;
+        System.out.printf("%s recieved %d %% of total marks.\n", name, total);
     }
 }
--- first_java_application/Week5.java
+++ first_java_application/Week5.java
@@ -1,44 +1,31 @@
-/*
-Programmer: Mason Larcombe s0257989
-File: Week5
-Date: 09/04/2017
-Purpose: COIT11222 assignment one question one for Term 1 2017
-using a secondary method, determine 9 student grades and the average mark.
-*/
-import javax.swing.JOptionPane;																				//Imported the popup dialog boxes used throughout the program
-public class Week5
-{
-    private static String getGrade(int mark)																//The getGrade method
-	{
-	    if (mark<=49)                                                                               		//If student scores 49 or lower, next line will execute
-        	JOptionPane.showMessageDialog(null, "Student has failed:(F)");
-	    else if (mark<=64)                                                                          		//If student scores between 50 and 64, next line will execute
-	        JOptionPane.showMessageDialog(null, "Student has passed:(P)");
-	    else if (mark<=74)                                                                          		//If student scores between 65 and 74, next line will execute
-	        JOptionPane.showMessageDialog(null, "Student has recieved a Credit:(C)");
-	    else if (mark<=84)                                                                         			//If student scores between 75 and 84, next line will execute
-	        JOptionPane.showMessageDialog(null, "Student has recieved a Distinction:(D)");
-	    else                                                                                        		//If student scores 85 of above, next line will execute
+import javax.swing.JOptionPane;
+
+public class Week5 {
+    private static void getGrade(int mark) {
+        if (mark <= 49)
+            JOptionPane.showMessageDialog(null, "Student has failed:(F)");
+        else if (mark <= 64)
+            JOptionPane.showMessageDialog(null, "Student has passed:(P)");
+        else if (mark <= 74)
+            JOptionPane.showMessageDialog(null, "Student has recieved a Credit:(C)");
+        else if (mark <= 84)
+            JOptionPane.showMessageDialog(null, "Student has recieved a Distinction:(D)");
+        else
             JOptionPane.showMessageDialog(null, "Student has recieved a High Distinction:(HD)");
+    }
 
-            return (String)(""+mark);																		//Turns the mark from an integer to a string
-        }
-
-   public static void main(String[] args)
-    {
-        int mark,tMark=0,count;																				//students mark, total mark and loop count variables
-        final int N=3;																						//Final variable, largest number of student ID
-        float avgMark;																						//Average student marks variable, float for decimal points
+    public static void main(String[] args) {
+        int mark, tMark = 0, count;
+        final int N = 3;
+        float avgMark;
 
         JOptionPane.showMessageDialog(null, "Welcome to the Mark Entry System");
-
-        for (count=N;count>0;count--)																		//A loop for the amount of student marks that need to be entered
-        {
-            mark=Integer.parseInt(JOptionPane.showInputDialog(null, "Enter Student mark (0-100)"));			//Popup dialog, asks for students mark between 0 and 100
-            getGrade(mark);																					//Sends the mark to the getGrade method
-            tMark=tMark+mark;																				//calculates the total mark, by adding the last entered mark to the total mark
+        for (count = N; count > 0; count--) {
+            mark = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter Student mark (0-100)"));
+            getGrade(mark);
+            tMark = tMark + mark;
         }
-        avgMark=(float)tMark/N;																				//Calculates average mark by dividing total mark by number of student results entered
-        JOptionPane.showMessageDialog(null, String.format("Average student mark is "+"%.2f",avgMark));		//Outputs a dialog box, showing average student mark upto two decimal places.
+        avgMark = (float) tMark / N;
+        JOptionPane.showMessageDialog(null, String.format("Average student mark is %.2f", avgMark));
     }
 }

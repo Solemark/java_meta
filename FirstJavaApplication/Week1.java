f4a531d8c07a2fcbdd43d7a79215789afa15d4a7
Solemark <larco9mason@gmail.com>
2023-07-16 11:34:10 +1000

    culled to 5 lines

--- FirstJavaApplication/Week1.java
+++ FirstJavaApplication/Week1.java
@@ -1,11 +1,9 @@
 public class Week1 {
     public static void main(String[] args) {
-        System.out.println("*     *  *");
-        System.out.println("**   **  *");
-        System.out.println("* * * *  *");
-        System.out.println("*  *  *  *");
-        System.out.println("*     *  *");
-        System.out.println("*     *  *");
-        System.out.println("*     *  *******");
+        System.out.println("*   *  *");
+        System.out.println("** **  *");
+        System.out.println("* * *  *");
+        System.out.println("*   *  *");
+        System.out.println("*   *  *****");
     }
 }
